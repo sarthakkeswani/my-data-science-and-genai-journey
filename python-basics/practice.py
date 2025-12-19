c0ee56bd438ac647a7a3e2232ec60766f29ba638
Sarthak Keswani <sarthakkeswani64@gmail.com>
2025-12-19 11:43:18 +0530

    Module 6: ML fundamentals with visual intuition and evaluation

---
+++ python-basics/practice.py
@@ -0,0 +1,25 @@
+'''age=int(input("Enter your age :"))
+if age>18:
+    print("you are eligible for vote. ")'''
+
+'''num1=int(input("enter num 1"))
+num2=int(input("enter num 2"))
+if num1>num2:
+    print("num 1 is largest")
+    
+else:
+    print("num 2 is largest")'''
+
+'''num1=int(input("enter a number : "))
+if num1%2==0:
+    print("number is even")
+
+else:
+    print("number is even")'''
+
+'''3 4 5'''
+
+a=int(input("Enter side 1"))
+b=int(input("Enter side 2"))
+c=int(input("Enter side 3"))
+
